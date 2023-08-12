ab30f3d6f1dbb1c5f7453e4fd9687301a4a59951
yamamoto-yuta <automatuX78@gmail.com>
2023-08-12 17:38:15 +0900

    [WIP] ArticleList まで実装

---
+++ src/components/pages/articles/ArticleList.tsx
@@ -0,0 +1,14 @@
+import { ConfigJson, defaultConfigJson, configJsonPath } from '../configJson'
+
+export const ArticleList = ({
+  configJson,
+}: {
+  configJson: ConfigJson
+}) => {
+  return (
+    <>
+      <h1>Article List</h1>
+      <div>aaa</div>
+    </>
+  )
+}
--- src/src/pages/articles.tsx
+++ src/src/pages/articles.tsx
@@ -1,31 +1,28 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { HomePage } from '../../components/pages/fixed/home/HomePage'
 import { readJson } from '../../lib/common/readJson'
-import { HomeJson, defaultHomeJson, homeJsonPath } from '../../components/pages/fixed/home/homeJson'
 import { ConfigJson, defaultConfigJson, configJsonPath } from '../../components/pages/configJson'
+import { ArticleList } from '../../components/pages/articles/ArticleList'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = ({
-  configJson,
-  homeJson
+  configJson
 }: {
-  configJson: ConfigJson,
-  homeJson: HomeJson
+  configJson: ConfigJson
 }) => {
   return (
     <>
       <Head>
-        <title>{`Home | ${configJson.blog_title}`}</title>
+        <title>{`Article List | ${configJson.blog_title}`}</title>
         <meta name="description" content={`${configJson.site_introduction}`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${inter.className}`}>
-        <HomePage
-          homeJson={homeJson}
+        <ArticleList
+          configJson={configJson}
         />
       </main>
     </>
@@ -34,9 +31,8 @@ const Home = ({
 
 export const getStaticProps = async () => {
   const configJson = readJson<ConfigJson>(configJsonPath, defaultConfigJson);
-  const homeJson = readJson<HomeJson>(homeJsonPath, defaultHomeJson);
   return {
-    props: { configJson, homeJson }
+    props: { configJson }
   }
 };
 
