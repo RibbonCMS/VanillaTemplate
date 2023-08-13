2110d2bd297ec1ae50842ac49c328e8b1defb16d
yamamoto-yuta <automatuX78@gmail.com>
2023-08-14 01:13:53 +0900

    [FIX] basePath を修正

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { basePath } = require('./lib/consts')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  basePath: basePath,
 }
 
 module.exports = nextConfig
